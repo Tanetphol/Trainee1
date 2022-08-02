201fe75b1bfa0c37da5e0798432d1f63c7c95207
tanetphol <tanetphol.sae@gmail.com>
2022-08-02 10:42:18 +0700

    first commit

---
+++ go.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+var input string = "tanetpho0@"
+
+func main() {
+	for _, r := range input {
+		if unicode.IsLetter(r) {
+			fmt.Println("alphabet checked")
+		} else if unicode.IsNumber(r) {
+			fmt.Println("number checked")
+		} else {
+			fmt.Println("nor number or alphabet")
+		}
+
+	}
+}
