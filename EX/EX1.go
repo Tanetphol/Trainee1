662acc486c77388a6cf086e58a37303d2493395a
tanetphol <tanetphol.sa@tnis.com>
2022-08-04 15:00:32 +0700

    [refactor]

--- EX/EX1.go
+++ EX/EX1.go
@@ -10,22 +10,50 @@ import (
 var input string = "h99 ส!"
 
 func main() {
-	sum := new(int)
+	sum := 0
 	defer func() {
-		fmt.Print(*sum)
+		sum = reverseNumber(sum)
+		fmt.Print(sum)
+
 	}()
 	input = strings.Replace(input, " ", "", -1)
 	for _, r := range input {
 		if r >= 'ก' && r <= 'ฮ' {
-			fmt.Printf("ก,ข,ฃ,ค,ฅ,ฆ,ง,จ")
+			writethai('ก', 'จ')
 		} else if (r > 'a' && r < 'z') || (r > 'A' && r < 'Z') {
-			fmt.Printf("A,B,C,D,E,F,G,H,I,J")
+			writeeng('A', 'J')
 		} else if unicode.IsPunct(r) {
 			fmt.Printf("%c", r)
 		} else if unicode.IsNumber(r) {
 			str := string(r)
 			int1, _ := strconv.Atoi(str)
-			*sum += int1
+			sum += int1
 		}
 	}
+}
+func reverseNumber(num int) int {
+
+	res := 0
+	for num > 0 {
+		remainder := num % 10
+		res = (res * 10) + remainder
+		num /= 10
+	}
+	return res
+}
+func writeeng(a, b rune) {
+	var re []rune
+	for ch := a; ch <= b; ch++ {
+		re = append(re, ch)
+	}
+	re2 := string(re)
+	fmt.Print(re2)
+}
+func writethai(a, b rune) {
+	var re []rune
+	for ch := a; ch <= b; ch++ {
+		re = append(re, ch)
+	}
+	re2 := string(re)
+	fmt.Print(re2)
 }
